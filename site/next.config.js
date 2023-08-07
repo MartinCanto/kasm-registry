690a19a7b85813c9fb02938d85e2e1989c814b1c
MartinCanto <joeymartin77@hotmail.com>
2023-08-07 11:25:16 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'MartinCanto/Kasm Technologies',
+    description: 'The MartinCanto store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://github.com/MartinCanto/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
